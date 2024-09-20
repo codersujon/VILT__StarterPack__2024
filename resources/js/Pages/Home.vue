fe46eb4366f2f3cc448c966b2c8416175b013ea3
unknown <csesujon155@gmail.com>
2024-09-20 12:46:43 -0700

    VILT Ready Starter Pack

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col h-screen items-center justify-center">
+        <h1 class="text-5xl md:text-7xl text-center py-6 text-blue-600">
+            VILT Starter Pack 2024
+        </h1>
+        <caption class="text-3xl text-slate-700">Created_At: 20 Sep, 2024</caption>
+    </div>
+</template>
