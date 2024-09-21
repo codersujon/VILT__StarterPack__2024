ad10c22413d4426e40507c0424e50b8bb9dc7fe2
unknown <csesujon155@gmail.com>
2024-09-21 09:00:56 -0700

    Ziggy Package Integrate for laravel vue name routes

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -27,7 +27,7 @@
                     </div>
                     <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div class="flex flex-shrink-0 items-center">
-                           <Link href="/">
+                           <Link :href="route('home')">
                             <img class="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                                 alt="Tailwind">
@@ -35,15 +35,15 @@
                         </div>
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
-                                <Link href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                                <Link :href="route('home')" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
                                     aria-current="page">Home</Link>
-                                <Link href="/about"
+                                <Link :href="route('about')"
                                     class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</Link>
-                                <Link href="/services"
+                                <Link :href="route('services')"
                                     class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Services</Link>
-                                <Link href="/blog"
+                                <Link :href="route('blog')"
                                     class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</Link>
-                                <Link href="/contact"
+                                <Link :href="route('contact')"
                                     class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</Link>
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,18 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home', ['title' => 'Home']);
-});
+})->name('home');
 
 # Way: 1
 Route::get('/about', function(){
     return Inertia::render('About', ['title' => 'About']);
-});
+})->name('about');
 
 # Way: 2 // using helper function
 Route::get('/blog', function(){
     return inertia('Blog', ['title' => 'Blog']); 
-});
+})->name('blog');
 
 # Way: 3
-Route::inertia('/services', 'Services', ['title' => "Services"]);  
-Route::inertia('/contact', 'Contact', ['title' => "Contact"]);
+Route::inertia('/services', 'Services', ['title' => "Services"])->name('services');  
+Route::inertia('/contact', 'Contact', ['title' => "Contact"])->name('contact');
