2ad799c20587d0bb8cefecaf230afb798f91b81d
unknown <csesujon155@gmail.com>
2024-09-20 23:35:30 -0700

    Blue Layout change for about page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,6 +1,10 @@
 <script setup>
-
+import BlueLayout from "../Layouts/BlueLayout.vue"
+defineOptions({
+    layout: BlueLayout
+});
 </script>
+
 <template>
      <div class="container mx-auto">
         <h1 class="text-5xl md:text-7xl text-center py-6 text-blue-600">
